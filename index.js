1f339e6428cf1372bbc59a605b1c88e962c0a521
TBarontoTI <tbarontoti@gmail.com>
2022-11-03 20:37:50 -0300

    Aula 07 Variaveis exercicios resolvidos

---
+++ index.js
@@ -0,0 +1,40 @@
+// Parte 1 dos Exercícios
+
+
+const nome = prompt("Qual o seu nome?")
+let idade = Number(prompt("Qual é a sua idade?"))
+
+console.log (nome)
+console.log (idade)
+
+console.log (idade.toString)
+
+console.log("Olá, meu nome é", nome, "e eu tenho", idade, "anos")
+
+console.log(typeof nome, nome)
+console.log(typeof idade, idade)
+
+let temPetOuNao = confirm ("Você tem Pet?")
+console.log(temPetOuNao)
+
+let nomePet
+
+if(temPetOuNao = true){
+    nomePet = prompt("Qual o nome do seu pet?")
+    
+}
+console.log(typeof nomePet, nomePet)
+
+
+// Parte 2 dos Exercícios
+
+const objeto = prompt ("Qual é o seu livro preferido?")
+let numObjeto = Number(prompt ("Quantas páginas o livro tem?"))
+let leitura = confirm ("Você lê livro semanalmente?")
+
+console.log (typeof objeto, objeto)
+console.log (typeof numObjeto, numObjeto)
+console.log (typeof leitura, leitura)
+
+// Parte 3 dos Exercícios
+
